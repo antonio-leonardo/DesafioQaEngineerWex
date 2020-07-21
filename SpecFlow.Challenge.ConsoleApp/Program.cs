cdd51c06e40e65889ce050945baeef45d1dbf5c1
Antonio Leonardo de Abreu Freire <antonio.leonardo@outlook.com.br>
2020-07-21 10:44:01 -0300

    Inclusão de Console para execução de testes em modo debug

--- SpecFlow.Challenge.ConsoleApp/Program.cs
+++ SpecFlow.Challenge.ConsoleApp/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Threading;
 using System.Xml.Serialization;
 
-namespace QaEngineerWex.ConsoleApp
+namespace SpecFlow.Challenge.ConsoleApp
 {
     class Program
     {
